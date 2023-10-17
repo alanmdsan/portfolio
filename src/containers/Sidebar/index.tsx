0a1db4db48cc337bfd787ea8011834e341f26639
alanmdsan <alanmdsan@gmail.com>
2023-10-17 17:55:34 -0300

    setting Titulo and Paragrafo components

---
+++ src/containers/Sidebar/index.tsx
@@ -0,0 +1,10 @@
+import Titulo from '../../components/Titulo'
+
+const Sidebar = () => (
+  <aside>
+    <img src="https://github.com/alanmdsan.png" alt="" />
+    <Titulo fontSize={20}>Alan Santos</Titulo>
+  </aside>
+)
+
+export default Sidebar
---
+++ src/containers/Sobre/index.tsx
@@ -0,0 +1,15 @@
+import Titulo from '../../components/Titulo'
+import Paragrafo from '../../components/Paragrafo'
+
+const Sobre = () => (
+  <section>
+    <Titulo fontSize={16}>Sobre mim</Titulo>
+    <Paragrafo tipo="principal">
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis velit
+      tempore labore, natus quasi vel. Unde qui similique porro omnis. Neque ex
+      atque sed, soluta ipsa blanditiis numquam doloremque dolor.
+    </Paragrafo>
+  </section>
+)
+
+export default Sobre
